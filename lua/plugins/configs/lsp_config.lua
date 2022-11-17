2ff44cbf83df67a96fb0e92e0450fc2e96f13ee9
Oscar Ruiz <oscaralexruiz@gmail.com>
2022-11-17 11:26:45 -0500

    [fix] solve issue with js LSP and declaration modules

--- lua/plugins/configs/lsp_config.lua
+++ lua/plugins/configs/lsp_config.lua
@@ -1,8 +1,27 @@
 return function()
+    -- TODO: Separate this LSP configs to independient files
     local LSP_SERVERS = {
-        lua = "sumneko_lua",
-        python = 'pyright',
-        ['js/ts'] = 'tsserver'
+        lua = {
+            server = "sumneko_lua",
+            settings = {}
+        },
+        python = {
+            server = 'pyright',
+            settings = {}
+        },
+        ['js/ts'] = {
+            server = 'tsserver',
+            settings = {
+                diagnostics = {
+					ignoredCodes = {
+						-- https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+
+                        -- TODO: Look for a way to apply this only for JS and not TS
+						7016, -- ts7016: Could not find a declaration file for module .
+					},
+				},
+            }
+        }
     }
 
     local ok_capabilities, cmp_nvim_lsp = pcall(require,'cmp_nvim_lsp')
@@ -13,9 +32,10 @@ return function()
 
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    for _, lsp_server in pairs(LSP_SERVERS) do
-        lspconfig[lsp_server].setup({
+    for _, lsp_definition in pairs(LSP_SERVERS) do
+        lspconfig[lsp_definition['server']].setup({
             capabilities = capabilities,
+            settings = lsp_definition['settings']
         })
     end
 end

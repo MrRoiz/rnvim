642f3e82ba82a577893520a8aa215e11eb0e891a
Oscar Ruiz <oscaralexruiz@gmail.com>
2022-11-17 11:42:55 -0500

    [fix] apply on_attach function to enable goto definition and other lsp commands

--- lua/plugins/configs/lsp_config.lua
+++ lua/plugins/configs/lsp_config.lua
@@ -24,6 +24,30 @@ return function()
         }
     }
 
+    local on_attach = function(client, bufnr)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        -- Mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+        -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        -- vim.keymap.set('n', '<space>wl', function()
+            -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            -- end, bufopts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+        -- vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        -- vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+    end
+
     local ok_capabilities, cmp_nvim_lsp = pcall(require,'cmp_nvim_lsp')
     local ok_lspconfig, lspconfig = pcall(require, 'lspconfig')
     if not ok_capabilities or not ok_lspconfig then
@@ -34,6 +58,7 @@ return function()
 
     for _, lsp_definition in pairs(LSP_SERVERS) do
         lspconfig[lsp_definition['server']].setup({
+            on_attach = on_attach,
             capabilities = capabilities,
             settings = lsp_definition['settings']
         })

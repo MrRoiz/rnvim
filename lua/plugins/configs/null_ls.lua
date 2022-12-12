b2fa5a028af072558bd888d07c9d83faeb31f2cf
Oscar Ruiz <oscaralexruiz@gmail.com>
2022-12-12 08:57:59 -0500

    fix(eslint_d): avoid to load eslint_d when there are not eslint config

--- lua/plugins/configs/null_ls.lua
+++ lua/plugins/configs/null_ls.lua
@@ -1,6 +1,7 @@
 return function()
 	local ok, null_ls = pcall(require, "null-ls")
-	if not ok then
+	local ok_utils, utils = pcall(require, "null-ls.utils")
+	if not ok or not ok_utils then
 		return
 	end
 
@@ -15,12 +16,27 @@ return function()
 			-- code_actions.cspell,
 
 			-- Diagnostics
-			diagnostics.eslint_d,
+			diagnostics.eslint_d.with({
+				filetypes = { "javascript", "typescript", "vue", "html", "css" },
+				condition = function()
+					return utils.root_pattern(
+						"eslint.config.js",
+						-- https://eslint.org/docs/user-guide/configuring/configuration-files#configuration-file-formats
+						".eslintrc",
+						".eslintrc.js",
+						".eslintrc.cjs",
+						".eslintrc.yaml",
+						".eslintrc.yml",
+						".eslintrc.json",
+						"package.json"
+					)(vim.api.nvim_buf_get_name(0)) ~= nil
+				end,
+			}),
 
 			-- Formatters
 			formatting.prettier,
 			formatting.stylua,
-            formatting.sql_formatter,
+			formatting.sql_formatter,
 		},
 	})
 end

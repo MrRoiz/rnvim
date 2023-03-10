c3544da659d947aa536abe90502c64f4e53a3497
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-03-10 08:41:39 -0500

    feat(nvim-tree): open nvim-tree on start up

--- lua/core/initialize.lua
+++ lua/core/initialize.lua
@@ -1,3 +1,9 @@
 require('core/options')
 require("core/utils").load_mappings()
 require("plugins")
+
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })

befb4b5d78e5ed8a9cc6a8b916d2bf646fb9b024
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-04-16 18:52:07 -0500

    refactor: add wilder

---
+++ lua/plugins/wilder.lua
@@ -0,0 +1,23 @@
+return {
+    "gelguy/wilder.nvim",
+    opts = {
+        modes = { ':', '?' }
+    },
+    config = function(plugin, opts)
+        local wilder = require("wilder")
+
+        wilder.setup(opts)
+
+        wilder.set_option('renderer', wilder.popupmenu_renderer(
+            wilder.popupmenu_palette_theme({
+                -- 'single', 'double', 'rounded' or 'solid'
+                -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+                border = 'rounded',
+                max_height = '75%',  -- max height of the palette
+                min_height = 0,      -- set to the same as 'max_height' for a fixed height window
+                prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+                reverse = 0,         -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+            })
+        ))
+    end
+}

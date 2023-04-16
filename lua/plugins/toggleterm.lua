e7d2bd93abae6038cd832b1930858591729e0f25
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-04-16 11:38:22 -0500

    refactor: adapted toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+    "akinsho/toggleterm.nvim",
+    opts = {
+        float_opts = {
+            border = "curved",
+            width = 150,
+            height = 30,
+        }
+    }
+}

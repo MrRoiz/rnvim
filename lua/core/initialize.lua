e2cdc9ad87188ab63238649bd4e673f1a0c8c347
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-04-16 10:09:43 -0500

    refactor: migrate indent-blankline to lazy and treesitter

--- lua/core/initialize.lua
+++ lua/core/initialize.lua
@@ -1,12 +1,3 @@
 require('core/options')
 require("core/utils").load_mappings()
 require("core/lazy")
-
-local function open_nvim_tree()
-  local ok, api = pcall(require, "nvim-tree.api")
-  if ok then
-    api.tree.open()
-  end
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })

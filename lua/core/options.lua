47a21b81849fad1630d6963b1bfddaf10438508d
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-04-17 11:29:35 -0500

    fix: avoid to close first buffer when opening nvim

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,10 @@
 local opt = vim.opt
 local g = vim.g
 
+-- Avoid to open up Explore
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+
 opt.title = true
 opt.clipboard = "unnamedplus"
 
--- lua/plugins/alpha_nvim.lua
+++ lua/plugins/alpha_nvim.lua
@@ -1,30 +1,26 @@
 return {
-    "goolord/alpha-nvim",
-    dependencies = { "nvim-tree-nvim-web-devicons" },
-    opts = {
-        -- NOTE: ASCII generated here: https://patorjk.com/software/taag/#p=display&f=Larry%203D&t=rnvim
-        header = {
-            [[                                               ]],
-            [[                                               ]],
-            [[                           __                  ]],
-            [[  _ __    ___     __  __  /\_\     ___ ___     ]],
-            [[ /\`'__\/' _ `\  /\ \/\ \ \/\ \  /' __` __`\   ]],
-            [[ \ \ \/ /\ \/\ \ \ \ \_/ | \ \ \ /\ \/\ \/\ \  ]],
-            [[  \ \_\ \ \_\ \_\ \ \___/   \ \_\\ \_\ \_\ \_\ ]],
-            [[   \/_/  \/_/\/_/  \/__/     \/_/ \/_/\/_/\/_/ ]],
-            [[                                               ]],
-            [[                                               ]],
-            [[    - By Mr. Roiz                              ]],
-        },
-    },
-    config = function(plugin, opts)
-        local dashboard = require("alpha.themes.dashboard")
-
-        dashboard.section.header.val = opts.header
-
-        require("alpha").setup(dashboard.opts)
-
-        vim.cmd("bd")
-        vim.cmd("Alpha")
-    end,
+	"goolord/alpha-nvim",
+	dependencies = { "nvim-tree-nvim-web-devicons" },
+	opts = {
+		-- NOTE: ASCII generated here: https://patorjk.com/software/taag/#p=display&f=Larry%203D&t=rnvim
+		header = {
+			[[                                               ]],
+			[[                                               ]],
+			[[                           __                  ]],
+			[[  _ __    ___     __  __  /\_\     ___ ___     ]],
+			[[ /\`'__\/' _ `\  /\ \/\ \ \/\ \  /' __` __`\   ]],
+			[[ \ \ \/ /\ \/\ \ \ \ \_/ | \ \ \ /\ \/\ \/\ \  ]],
+			[[  \ \_\ \ \_\ \_\ \ \___/   \ \_\\ \_\ \_\ \_\ ]],
+			[[   \/_/  \/_/\/_/  \/__/     \/_/ \/_/\/_/\/_/ ]],
+			[[                                               ]],
+			[[                                               ]],
+			[[    - By Mr. Roiz                              ]],
+		},
+	},
+	config = function(plugin, opts)
+		local dashboard = require("alpha.themes.dashboard")
+		dashboard.section.header.val = opts.header
+		local alpha = require("alpha")
+		alpha.setup(dashboard.opts)
+	end,
 }

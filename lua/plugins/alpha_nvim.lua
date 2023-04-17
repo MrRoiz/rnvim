593050d507bf873d89b678f87590d62edc5e1648
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-04-17 08:30:58 -0500

    feat: custom header on dashboard

--- lua/plugins/alpha_nvim.lua
+++ lua/plugins/alpha_nvim.lua
@@ -1,9 +1,30 @@
 return {
     "goolord/alpha-nvim",
     dependencies = { "nvim-tree-nvim-web-devicons" },
-    config = function (plugin, opts)
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
+    opts = {
+        -- NOTE: ASCII generated here: https://patorjk.com/software/taag/#p=display&f=Larry%203D&t=rnvim
+        header = {
+            [[                                               ]],
+            [[                                               ]],
+            [[                           __                  ]],
+            [[  _ __    ___     __  __  /\_\     ___ ___     ]],
+            [[ /\`'__\/' _ `\  /\ \/\ \ \/\ \  /' __` __`\   ]],
+            [[ \ \ \/ /\ \/\ \ \ \ \_/ | \ \ \ /\ \/\ \/\ \  ]],
+            [[  \ \_\ \ \_\ \_\ \ \___/   \ \_\\ \_\ \_\ \_\ ]],
+            [[   \/_/  \/_/\/_/  \/__/     \/_/ \/_/\/_/\/_/ ]],
+            [[                                               ]],
+            [[                                               ]],
+            [[    - By Mr. Roiz                              ]],
+        },
+    },
+    config = function(plugin, opts)
+        local dashboard = require("alpha.themes.dashboard")
+
+        dashboard.section.header.val = opts.header
+
+        require("alpha").setup(dashboard.opts)
+
         vim.cmd("bd")
         vim.cmd("Alpha")
-    end
+    end,
 }

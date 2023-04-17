9449b7550a17dc6ca2ceba1050bb4e0a0583bb73
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-04-17 07:58:42 -0500

    feat: set lazy loading to avoid loading unnecesary plugins on start up

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -1,9 +1,10 @@
 return {
-    "pwntester/octo.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim",
-        "kyazdani42-nvim-web-devicons",
-    },
-    config = true
+	"pwntester/octo.nvim",
+	cmd = { "Octo" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
+		"kyazdani42-nvim-web-devicons",
+	},
+	config = true,
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,10 +1,11 @@
 return {
     "akinsho/toggleterm.nvim",
+    cmd = { "ToggleTerm" },
     opts = {
         float_opts = {
             border = "curved",
             width = 150,
             height = 30,
-        }
-    }
+        },
+    },
 }

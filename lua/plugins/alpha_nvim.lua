fc72df956d59cc4bb70057276632c591fad4968b
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-04-17 00:05:13 -0500

    refactor: close Explore before opening Alpha

--- lua/plugins/alpha_nvim.lua
+++ lua/plugins/alpha_nvim.lua
@@ -3,6 +3,7 @@ return {
     dependencies = { "nvim-tree-nvim-web-devicons" },
     config = function (plugin, opts)
         require'alpha'.setup(require'alpha.themes.dashboard'.config)
+        vim.cmd("bd")
         vim.cmd("Alpha")
     end
 }

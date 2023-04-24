9a5e40cf08fcb91648d6ed8aa8fa8375526f153a
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-04-24 08:25:45 -0500

    feat: setup noice pluging

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,38 @@
+return {
+	"folke/noice.nvim",
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		"rcarriga/nvim-notify",
+	},
+	opts = {
+		lsp = {
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			},
+		},
+		-- you can enable a preset for easier configuration
+		presets = {
+			bottom_search = false, -- use a classic bottom cmdline for search
+			command_palette = true, -- position the cmdline and popupmenu together
+			long_message_to_split = true, -- long messages will be sent to a split
+			inc_rename = true,   -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = true, -- add a border to hover docs and signature help
+		},
+
+		-- NOTE: Disabled becuase of some issues with nvim-tree when saving a file multiple times
+		messages = {
+			enabled = false,
+		},
+		notify = {
+			enabled = false,
+		},
+		-- NOTE: End note
+	},
+}

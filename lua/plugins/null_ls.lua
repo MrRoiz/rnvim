78bbdeaa98499426994f0c3aeb994980632668bb
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-04-26 17:07:00 -0500

    fix: remove eslint diagnostics to avoid duplicated errors with eslint lsp

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -2,9 +2,7 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function()
         local null_ls = require("null-ls")
-        local utils = require("null-ls.utils")
 
-        local diagnostics = null_ls.builtins.diagnostics
         local formatting = null_ls.builtins.formatting
         -- local code_actions = null_ls.builtins.code_actions
 
@@ -13,24 +11,6 @@ return {
                 -- TODO: Find a way to discard words on cspell or find other linter for spelling
                 -- diagnostics.cspell,
                 -- code_actions.cspell,
-
-                -- Diagnostics
-                diagnostics.eslint_d.with({
-                    filetypes = { "javascript", "typescript", "vue", "html", "css" },
-                    condition = function()
-                        return utils.root_pattern(
-                                "eslint.config.js",
-                                -- https://eslint.org/docs/user-guide/configuring/configuration-files#configuration-file-formats
-                                ".eslintrc",
-                                ".eslintrc.js",
-                                ".eslintrc.cjs",
-                                ".eslintrc.yaml",
-                                ".eslintrc.yml",
-                                ".eslintrc.json",
-                                "package.json"
-                            )(vim.api.nvim_buf_get_name(0)) ~= nil
-                    end,
-                }),
 
                 -- Formatters
                 formatting.prettier,

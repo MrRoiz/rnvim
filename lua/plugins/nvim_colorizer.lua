4aea1bc75d7b497b5118b21a7bd20aeba52a4ed0
Oscar Ruiz <oscaralexruiz@gmail.com>
2023-05-05 16:16:11 -0500

    fix: avoid to colorize on nvimtree

--- lua/plugins/nvim_colorizer.lua
+++ lua/plugins/nvim_colorizer.lua
@@ -1,9 +1,11 @@
 return {
 	"NvChad/nvim-colorizer.lua",
 	opts = {
+		-- TODO: Find a way to avoid colorizing on Octo an DiffView
 		filetypes = {
 			"*",
 			"!cmp_menu",
+			"!NvimTree",
 		},
 		user_default_options = {
 			tailwind = true,

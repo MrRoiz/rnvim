75296604392316c68e68bfa7c2a9730a06004e83
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-05-01 19:23:47 -0500

    feat: implement typescript-tools plugin

--- lua/core/languages/javascript.lua
+++ lua/core/languages/javascript.lua
@@ -1,27 +1,21 @@
 return {
-  lsp = {
-    {
-      server = "tsserver",
-      server_opts = {
-        settings = {
-          diagnostics = {
-            ignoredCodes = {
-              -- https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
-              -- TODO: Find a way to separate this option to apply it only for Javascript
-              7016, -- ts7016: No declaration file
-            },
-          },
-        },
-      },
-    },
-    {
-      -- TODO: Find a way to attach this lsp only when the project has tailwind
-      server = "tailwindcss",
-    },
-    {
-      server = "eslint"
-    }
-  },
-  treesitter = { "tsx", "javascript", "typescript", "jsdoc", "prisma" },
-  formatter = "prettierd",
+	lsp = {
+		{
+			server = "tsserver",
+			plugin = {
+				"pmizio/typescript-tools.nvim",
+				dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+				opts = {},
+			},
+		},
+		{
+			-- TODO: Find a way to attach this lsp only when the project has tailwind
+			server = "tailwindcss",
+		},
+		{
+			server = "eslint",
+		},
+	},
+	treesitter = { "tsx", "javascript", "typescript", "jsdoc", "prisma" },
+	formatter = "prettierd",
 }

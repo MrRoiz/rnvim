369c0482583a763d343d6f115b0314ed6bc320b5
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-05-10 18:00:55 -0500

    feat: apply noice config similar to lazy vim

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -10,35 +10,31 @@ return {
 	},
 	opts = {
 		lsp = {
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
 				["cmp.entry.get_documentation"] = true,
 			},
 		},
-		-- you can enable a preset for easier configuration
-		presets = {
-			bottom_search = false, -- use a classic bottom cmdline for search
-			command_palette = true, -- position the cmdline and popupmenu together
-			long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = true,   -- enables an input dialog for inc-rename.nvim
-			lsp_doc_border = true, -- add a border to hover docs and signature help
-		},
-		-- NOTE: Disabled becuase of some issues with nvim-tree when saving a file multiple times
-		views = {
-			mini = {
-				position = {
-					row = -2,
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					any = {
+						{ find = "%d+L, %d+B" },
+						{ find = "; after #%d+" },
+						{ find = "; before #%d+" },
+					},
 				},
+				view = "mini",
 			},
 		},
-		messages = {
-			enabled = false,
+		presets = {
+			bottom_search = false,
+			command_palette = true,
+			long_message_to_split = true,
+			inc_rename = true,
+			lsp_doc_border = true,
 		},
-		notify = {
-			enabled = false,
-		},
-		-- NOTE: End note
 	},
 }

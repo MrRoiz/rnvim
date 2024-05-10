f4b8bab320a83e99a7969d81e0fe89adb1f614b3
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-05-09 19:20:55 -0500

    fix: only attach tailwind lsp when it is configured in the project

--- lua/core/languages/javascript.lua
+++ lua/core/languages/javascript.lua
@@ -9,8 +9,21 @@ return {
 			},
 		},
 		{
-			-- TODO: Find a way to attach this lsp only when the project has tailwind
 			server = "tailwindcss",
+			server_opts = {
+				-- This config will avoid to trigger tailwindcss lsp when it is not configured in the project
+				root_dir = function(fname)
+					local util = require("lspconfig/util")
+					return util.root_pattern(
+						"tailwind.config.js",
+						"tailwind.config.ts",
+						"tailwind.config.cjs",
+						"tailwind.js",
+						"tailwind.ts",
+						"tailwind.cjs"
+					)(fname)
+				end,
+			},
 		},
 		{
 			server = "eslint",

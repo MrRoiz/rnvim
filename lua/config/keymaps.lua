c60c86fdcc962121997d30a606db56a3cf85c7b2
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-05-29 15:46:27 -0500

    feat: add some other missing keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,6 @@ vim.keymap.set('v', '<S-Tab>', '<gv', { silent = true, desc = 'Remove indent to 
 
 vim.keymap.set({ 'n', 'v' }, '<C-S-j>', '10j', { silent = true, desc = 'Move faster down' })
 vim.keymap.set({ 'n', 'v' }, '<C-S-k>', '10k', { silent = true, desc = 'Move faster up' })
+
+vim.keymap.set('i', '<C-b>', '<esc>^i', { silent = true, desc = 'Go to the beggining of the line in insert mode' })
+vim.keymap.set('i', '<C-e>', '<end>', { silent = true, desc = 'Go to the end of the line in insert mode' })

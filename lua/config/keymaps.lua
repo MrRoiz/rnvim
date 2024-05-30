fb52e185f07cbb8b90439fd354a0c1e41183a1ef
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-05-30 17:19:58 -0500

    feat: customize lazyterm mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,24 @@ vim.keymap.set({ 'n', 'v' }, '<C-S-k>', '10k', { silent = true, desc = 'Move fas
 
 vim.keymap.set('i', '<C-b>', '<esc>^i', { silent = true, desc = 'Go to the beggining of the line in insert mode' })
 vim.keymap.set('i', '<C-e>', '<end>', { silent = true, desc = 'Go to the end of the line in insert mode' })
+
+-- floating remaping terminal start
+local lazyterm = function()
+  LazyVim.terminal(nil, {
+    cwd = LazyVim.root(),
+    size = {
+      width = 150,
+      height = 30,
+    },
+    border = 'rounded',
+  })
+end
+
+vim.keymap.del('n', '<leader>ft')
+vim.keymap.del('n', '<leader>fT')
+vim.keymap.del('n', '<c-/>')
+vim.keymap.del('n', '<c-_>')
+
+vim.keymap.set('n', '<leader>ft', lazyterm, { desc = 'Terminal (Root Dir)' })
+
+-- floating remaping terminal end

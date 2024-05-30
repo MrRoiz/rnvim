a8b9222dc02836ee1f375b54f17e9f4d62604f22
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-05-30 13:46:40 -0500

    fix: make diffview keymaps silent

--- lua/plugins/coding/diffview.lua
+++ lua/plugins/coding/diffview.lua
@@ -4,10 +4,15 @@ return {
     'nvim-lua/plenary.nvim',
   },
   keys = {
-    { '<Leader>dv', ':DiffviewOpen<CR>' },
-    { '<Leader>dcc', '<cmd>set hidden<CR><cmd>DiffviewClose<CR><cmd>set nohidden<CR>' },
-    { '<Leader>dh', ':DiffviewFileHistory<CR>' },
-    { '<Leader>dch', ':DiffviewFileHistory %<CR>' },
+    { '<Leader>dv', '<cmd>DiffviewOpen<CR>', desc = 'Open Diffview', silent = true },
+    {
+      '<Leader>dcc',
+      '<cmd>set hidden<CR><cmd>DiffviewClose<CR><cmd>set nohidden<CR>',
+      desc = 'Close Diffview',
+      silent = true,
+    },
+    { '<Leader>dh', '<cmd>DiffviewFileHistory<CR>', desc = 'Open Diffview file history', silent = true },
+    { '<Leader>dch', '<cmd>DiffviewFileHistory %<CR>', desc = 'Open Diffview current file history', silent = true },
   },
   cmd = {
     'DiffviewClose',

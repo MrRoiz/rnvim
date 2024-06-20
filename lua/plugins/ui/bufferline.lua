bb554671d0f0c3c62ca42213bb98194f194b687e
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-06-20 09:17:30 -0500

    fix: add offset to diffview with bufferline

--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,8 +1,22 @@
 return {
   'akinsho/bufferline.nvim',
-  opts = {
-    options = {
-      always_show_bufferline = true,
-    },
-  },
+  opts = function(_, opts)
+    opts.options.always_show_bufferline = true
+
+    opts.options.offsets = {
+      {
+        filetype = 'neo-tree',
+        text = 'File Explorer',
+        highlight = 'Directory',
+        text_align = 'center',
+        separator = true,
+      },
+      {
+        filetype = 'DiffviewFiles',
+        text = 'Source Control',
+        text_align = 'center',
+        separator = true,
+      },
+    }
+  end,
 }

cdb3b159aef720bad788e3dbce059969c1952627
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-07-29 10:04:15 -0500

    feat: reuse vtsls and implement eslint

--- lua/plugins/languages/typescript-backup.lua
+++ lua/plugins/languages/typescript-backup.lua
@@ -1,7 +1,35 @@
--- NOTE: This is a backup to quickly enable vtsls lsp it NEEDS to enable first the typescript extra in lazyvim
 return {
   'neovim/nvim-lspconfig',
-  opts = function(_, opts)
-    opts.servers.vtsls.settings.typescript = {}
-  end,
+  dependencies = {
+    {
+      'pmizio/typescript-tools.nvim',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'neovim/nvim-lspconfig',
+      },
+    },
+  },
+  opts = {
+    servers = {
+      tsserver = {
+        on_attach = function()
+          LazyVim.lsp.on_attach(function(client, buffer)
+            require('lazyvim.plugins.lsp.keymaps').on_attach(client, buffer)
+          end)
+        end,
+      },
+      eslint = {
+        settings = {
+          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+          workingDirectories = { mode = 'auto' },
+        },
+      },
+    },
+    setup = {
+      tsserver = function(_, opts)
+        require('typescript-tools').setup(opts)
+        return true
+      end,
+    },
+  },
 }
--- lua/plugins/languages/typescript.lua
+++ lua/plugins/languages/typescript.lua
@@ -1,35 +1,13 @@
+-- NOTE: This is a backup to quickly enable vtsls lsp it NEEDS to enable first the typescript extra in lazyvim
 return {
   'neovim/nvim-lspconfig',
-  dependencies = {
-    {
-      'pmizio/typescript-tools.nvim',
-      dependencies = {
-        'nvim-lua/plenary.nvim',
-        'neovim/nvim-lspconfig',
+  opts = function(_, opts)
+    opts.servers.vtsls.settings.typescript = {}
+    opts.servers.eslint = {
+      settings = {
+        -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+        workingDirectories = { mode = 'auto' },
       },
-    },
-  },
-  opts = {
-    servers = {
-      tsserver = {
-        on_attach = function()
-          LazyVim.lsp.on_attach(function(client, buffer)
-            require('lazyvim.plugins.lsp.keymaps').on_attach(client, buffer)
-          end)
-        end,
-      },
-      eslint = {
-        settings = {
-          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
-          workingDirectories = { mode = 'auto' },
-        },
-      },
-    },
-    setup = {
-      tsserver = function(_, opts)
-        require('typescript-tools').setup(opts)
-        return true
-      end,
-    },
-  },
+    }
+  end,
 }

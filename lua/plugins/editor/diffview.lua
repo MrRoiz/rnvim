89e92538527c569d4e4314203cc8e85089915cbe
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-08-14 10:13:31 -0500

    fix: remap keymaps for diffview to avoid conflicts

--- lua/plugins/editor/diffview.lua
+++ lua/plugins/editor/diffview.lua
@@ -23,11 +23,31 @@ return {
     'DiffviewRefresh',
     'DiffviewToggleFiles',
   },
-  opts = {
-    view = {
-      merge_tool = {
-        layout = 'diff3_mixed',
+  opts = function()
+    local actions = require('diffview.actions')
+    return {
+      view = {
+        merge_tool = {
+          layout = 'diff3_mixed',
+        },
       },
-    },
-  },
+      keymaps = {
+        view = {
+          -- NOTE: Added a `d` key at the beggining of each keymap to avoid conflicts
+          -- stylua: ignore start
+          { "n", "<leader>dco",  actions.conflict_choose("ours"),        { desc = "Choose the OURS version of a conflict" } },
+          { "n", "<leader>dct",  actions.conflict_choose("theirs"),      { desc = "Choose the THEIRS version of a conflict" } },
+          { "n", "<leader>dcb",  actions.conflict_choose("base"),        { desc = "Choose the BASE version of a conflict" } },
+          { "n", "<leader>dca",  actions.conflict_choose("all"),         { desc = "Choose all the versions of a conflict" } },
+          { "n", "<leader>ddx",  actions.conflict_choose("none"),        { desc = "Delete the conflict region" } },
+          { "n", "<leader>dcO",  actions.conflict_choose_all("ours"),    { desc = "Choose the OURS version of a conflict for the whole file" } },
+          { "n", "<leader>dcT",  actions.conflict_choose_all("theirs"),  { desc = "Choose the THEIRS version of a conflict for the whole file" } },
+          { "n", "<leader>dcB",  actions.conflict_choose_all("base"),    { desc = "Choose the BASE version of a conflict for the whole file" } },
+          { "n", "<leader>dcA",  actions.conflict_choose_all("all"),     { desc = "Choose all the versions of a conflict for the whole file" } },
+          { "n", "<leader>ddX",  actions.conflict_choose_all("none"),    { desc = "Delete the conflict region for the whole file" } },
+          -- stylua: ignore end
+        },
+      },
+    }
+  end,
 }

9fadb61b65e7ee426ac0a2cf3a91cf6ed92ff300
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-09-10 09:16:14 -0500

    chore: disable unused colorschemes

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,5 +1,10 @@
 return {
-  { 'Shatur/neovim-ayu' },
+  -- Disable unused colorshemes
+  { 'folke/tokyonight.nvim', enabled = false },
+  { 'catppuccin/nvim', enabled = false },
+
+  -- Config actual colorscheme used and lualine
+  { 'Shatur/neovim-ayu', lazy = true },
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'Shatur/neovim-ayu' },

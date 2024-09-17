4dd3fc389a3b448c3c2ee596d716bcd72fe02c38
Oscar Ruiz <oscaralexruiz@gmail.com>
2024-09-17 18:06:45 -0500

    feat: add better ts errors

--- lua/plugins/languages/typescript.lua
+++ lua/plugins/languages/typescript.lua
@@ -1,12 +1,30 @@
+local keymaps = {
+  toggle = '<leader>cbd', -- default '<leader>dd'
+  go_to_definition = '<leader>cbx', -- default '<leader>dx'
+}
+
 return {
-  'neovim/nvim-lspconfig',
-  opts = function(_, opts)
-    opts.servers.vtsls.settings.typescript = {}
-    opts.servers.eslint = {
-      settings = {
-        -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
-        workingDirectories = { mode = 'auto' },
-      },
-    }
-  end,
+  {
+    'neovim/nvim-lspconfig',
+    opts = function(_, opts)
+      opts.servers.vtsls.settings.typescript = {}
+      opts.servers.eslint = {
+        settings = {
+          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+          workingDirectories = { mode = 'auto' },
+        },
+      }
+    end,
+  },
+  {
+    'OlegGulevskyy/better-ts-errors.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim' },
+    keys = {
+      keymaps.toggle,
+      keymaps.go_to_definition,
+    },
+    opts = {
+      keymaps = keymaps,
+    },
+  },
 }

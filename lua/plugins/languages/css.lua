2db8a0a9c10adbf67506e4463424eb5075a47fc3
Oscar Ruiz <oscaralexruiz@gmail.com>
2025-07-10 16:17:52 -0500

    feat: add css lsp to the setup

--- lua/plugins/languages/css.lua
+++ lua/plugins/languages/css.lua
@@ -1,8 +1,11 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  opts = function(_, opts)
-    if type(opts.ensure_installed) == 'table' then
-      vim.list_extend(opts.ensure_installed, { 'css', 'scss' })
-    end
-  end,
+  { 'neovim/nvim-lspconfig', opts = { servers = { cssls = {} } } },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        vim.list_extend(opts.ensure_installed, { 'css', 'scss' })
+      end
+    end,
+  },
 }

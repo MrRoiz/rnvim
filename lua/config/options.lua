6062b63df03c0441bca31e101e7c0cae0b7fa721
oscaralexruiz@gmail.com <oscaralexruiz@gmail.com>
2025-08-02 17:20:53 -0500

    feat: enable markdown renderer and implement biome

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Enable this option to avoid conflicts with Prettier.
+vim.g.lazyvim_prettier_needs_config = true
+
 -- Add borders to diagnostic windows
 vim.diagnostic.config({
   float = { border = 'single' },
--- lua/plugins/languages/markdown.lua
+++ lua/plugins/languages/markdown.lua
@@ -7,13 +7,43 @@ return {
       end
     end,
   },
+  -- {
+  --   'iamcco/markdown-preview.nvim',
+  --   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  --   build = 'cd app && yarn install',
+  --   init = function()
+  --     vim.g.mkdp_filetypes = { 'markdown' }
+  --   end,
+  --   ft = { 'markdown' },
+  -- },
   {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && yarn install',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = false,
+      },
+    },
+    ft = { 'markdown', 'norg', 'rmd', 'org', 'codecompanion' },
+    config = function(_, opts)
+      require('render-markdown').setup(opts)
+      Snacks.toggle({
+        name = 'Render Markdown',
+        get = function()
+          return require('render-markdown.state').enabled
+        end,
+        set = function(enabled)
+          local m = require('render-markdown')
+          if enabled then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      }):map('<leader>um')
     end,
-    ft = { 'markdown' },
   },
 }
